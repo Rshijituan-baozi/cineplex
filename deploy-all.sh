4028784ea02f4601f72082655ddff04f9ffd50cd
Rshijituan <rshijituan@gmail.com>
2026-05-18 20:37:47 +0800

    fix: add X-Real-IP and X-Forwarded-For to nginx API proxy

--- deploy-all.sh
+++ deploy-all.sh
@@ -91,6 +91,8 @@ server {
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_read_timeout 86400s;
         proxy_send_timeout 86400s;
     }
@@ -107,6 +109,8 @@ server {
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_read_timeout 86400s;
         proxy_send_timeout 86400s;
     }
